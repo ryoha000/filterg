dcac2f5c91d9baad47fae64492729949bd770ec3
ryoha000 <the7uya@gmail.com>
2021-09-24 07:09:34 +0900

    deferでexitを送るように

--- process/src/capture.rs
+++ process/src/capture.rs
@@ -5,39 +5,50 @@ use super::utils::{CloseHandleOnExit, CoUninitializeOnExit};
 use bindings::Windows::Win32::Media::Multimedia::HMMIO;
 use bindings::Windows::Win32::System::Com::{CoInitializeEx, COINIT_MULTITHREADED};
 use bindings::Windows::Win32::{Foundation::HANDLE, Media::Audio::CoreAudio::IMMDevice};
-use std::ptr;
+use std::panic::panic_any;
+use std::sync::mpsc::Sender;
+use std::{fmt, ptr};
 
 pub struct Args {
     pub hr: windows::HRESULT,
     pub mm_device: IMMDevice,
     pub b_int16: bool,
     pub h_file: HMMIO,
-    pub h_started_event: HANDLE,
     pub h_stop_event: HANDLE,
     pub n_frames: u32,
 }
 
-pub fn capture() -> windows::Result<u8> {
+#[derive(Debug)]
+pub enum CaptureEvent {
+    Start,
+    Exit,
+}
+struct DeferChan {
+    tx: Sender<CaptureEvent>,
+}
+
+impl Drop for DeferChan {
+    fn drop(&mut self) {
+        let s = self.tx.send(CaptureEvent::Exit);
+        match s {
+            Ok(_) => {}
+            Err(e) => panic_any(e),
+        }
+    }
+}
+
+pub fn capture_thread_func(tx: Sender<CaptureEvent>) -> windows::Result<u8> {
+    let _defer = DeferChan { tx };
+
     unsafe { CoInitializeEx(ptr::null_mut(), COINIT_MULTITHREADED)? };
     let _com = CoUninitializeOnExit {};
 
-    let args = prepare()?;
+    // args を作成。TODO: 入力を受け取るようにする
 
-    println!("capture. ");
-    Ok(0)
-}
-
-fn prepare() -> windows::Result<Args> {
     let default_device = get_default_device()?;
     println!("default_device.GetId(): {:#?}", unsafe {
         default_device.GetId().unwrap()
     });
-
-    let h_started_event = create_event()?;
-    let _h_started_event = CloseHandleOnExit {
-        handle: h_started_event,
-    };
-    println!("h_started_event: {:#?}", h_started_event);
 
     let h_stop_event = create_event()?;
     let _h_stop_event = CloseHandleOnExit {
@@ -50,10 +61,10 @@ fn prepare() -> windows::Result<Args> {
         mm_device: default_device,
         b_int16: false,
         h_file: open_file("filterg_save.wav")?,
-        h_started_event,
         h_stop_event,
         n_frames: 0,
     };
 
-    Ok(args)
+    println!("capture. ");
+    Ok(0)
 }
--- process/src/lib.rs
+++ process/src/lib.rs
@@ -5,13 +5,12 @@ mod file;
 mod utils;
 
 use bindings::Windows::Win32::System::Com::{CoInitializeEx, COINIT_MULTITHREADED};
-use capture::Args;
-use device::get_default_device;
-use event::create_event;
+use capture::CaptureEvent;
+use std::sync::mpsc;
+use std::sync::mpsc::{Receiver, Sender};
 use std::{ptr, thread};
-use utils::{CloseHandleOnExit, CoUninitializeOnExit};
 
-use crate::file::open_file;
+use utils::{message_to_windows_error, CoUninitializeOnExit};
 
 pub fn wmain() -> windows::Result<u8> {
     unsafe { CoInitializeEx(ptr::null_mut(), COINIT_MULTITHREADED)? };
@@ -21,7 +20,22 @@ pub fn wmain() -> windows::Result<u8> {
 }
 
 fn do_everything() -> windows::Result<u8> {
-    let capture_thread = thread::spawn(move || capture::capture());
+    let (tx, rx): (Sender<CaptureEvent>, Receiver<CaptureEvent>) = mpsc::channel();
+
+    // TODO: 入力を処理して capture に渡すようにする
+    let capture_thread = thread::spawn(move || capture::capture_thread_func(tx));
+
+    match rx.recv() {
+        Ok(ev) => match ev {
+            CaptureEvent::Start => {} // start イベントなら次に進む
+            not_start_event => {
+                return Err(message_to_windows_error(&format!("{:#?}", not_start_event)));
+            }
+        },
+        Err(e) => {
+            return Err(message_to_windows_error(&format!("{:#?}", e)));
+        }
+    }
 
     Ok(0)
 }
