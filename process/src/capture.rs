43b7476e9dda38044accbc7eeb96891281910750
ryoha000 <the7uya@gmail.com>
2021-09-24 06:14:27 +0900

    capture スレッドで args をとるように

--- process/src/capture.rs
+++ process/src/capture.rs
@@ -1,5 +1,11 @@
+use super::device::get_default_device;
+use super::event::create_event;
+use super::file::open_file;
+use super::utils::{CloseHandleOnExit, CoUninitializeOnExit};
 use bindings::Windows::Win32::Media::Multimedia::HMMIO;
+use bindings::Windows::Win32::System::Com::{CoInitializeEx, COINIT_MULTITHREADED};
 use bindings::Windows::Win32::{Foundation::HANDLE, Media::Audio::CoreAudio::IMMDevice};
+use std::ptr;
 
 pub struct Args {
     pub hr: windows::HRESULT,
@@ -9,4 +15,45 @@ pub struct Args {
     pub h_started_event: HANDLE,
     pub h_stop_event: HANDLE,
     pub n_frames: u32,
+}
+
+pub fn capture() -> windows::Result<u8> {
+    unsafe { CoInitializeEx(ptr::null_mut(), COINIT_MULTITHREADED)? };
+    let _com = CoUninitializeOnExit {};
+
+    let args = prepare()?;
+
+    println!("capture. ");
+    Ok(0)
+}
+
+fn prepare() -> windows::Result<Args> {
+    let default_device = get_default_device()?;
+    println!("default_device.GetId(): {:#?}", unsafe {
+        default_device.GetId().unwrap()
+    });
+
+    let h_started_event = create_event()?;
+    let _h_started_event = CloseHandleOnExit {
+        handle: h_started_event,
+    };
+    println!("h_started_event: {:#?}", h_started_event);
+
+    let h_stop_event = create_event()?;
+    let _h_stop_event = CloseHandleOnExit {
+        handle: h_stop_event,
+    };
+    println!("h_stop_event: {:#?}", h_stop_event);
+
+    let args = Args {
+        hr: windows::HRESULT(0),
+        mm_device: default_device,
+        b_int16: false,
+        h_file: open_file("filterg_save.wav")?,
+        h_started_event,
+        h_stop_event,
+        n_frames: 0,
+    };
+
+    Ok(args)
 }
--- process/src/lib.rs
+++ process/src/lib.rs
@@ -4,12 +4,11 @@ mod event;
 mod file;
 mod utils;
 
-use bindings::Windows::Win32::Foundation::HANDLE;
 use bindings::Windows::Win32::System::Com::{CoInitializeEx, COINIT_MULTITHREADED};
 use capture::Args;
 use device::get_default_device;
 use event::create_event;
-use std::ptr;
+use std::{ptr, thread};
 use utils::{CloseHandleOnExit, CoUninitializeOnExit};
 
 use crate::file::open_file;
@@ -22,32 +21,7 @@ pub fn wmain() -> windows::Result<u8> {
 }
 
 fn do_everything() -> windows::Result<u8> {
-    let default_device = get_default_device()?;
-    println!("default_device.GetId(): {:#?}", unsafe {
-        default_device.GetId().unwrap()
-    });
-
-    let h_started_event = create_event()?;
-    let _h_started_event = CloseHandleOnExit {
-        handle: h_started_event,
-    };
-    println!("h_started_event: {:#?}", h_started_event);
-
-    let h_stop_event = create_event()?;
-    let _h_stop_event = CloseHandleOnExit {
-        handle: h_stop_event,
-    };
-    println!("h_stop_event: {:#?}", h_stop_event);
-
-    let args = Args {
-        hr: windows::HRESULT(0),
-        mm_device: default_device,
-        b_int16: false,
-        h_file: open_file("filterg_save.wav")?,
-        h_started_event,
-        h_stop_event,
-        n_frames: 0,
-    };
+    let capture_thread = thread::spawn(move || capture::capture());
 
     Ok(0)
 }
