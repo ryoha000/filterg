d73281214c334b249c25cc709a875faba0c920f4
ryoha000 <the7uya@gmail.com>
2021-09-24 08:54:04 +0900

    windows::Error::from_win32() を使うように

--- process/src/event.rs
+++ process/src/event.rs
@@ -1,15 +1,11 @@
 use bindings::Windows::Win32::Foundation::HANDLE;
-use bindings::Windows::Win32::System::Diagnostics::Debug::GetLastError;
 use bindings::Windows::Win32::System::Threading::CreateEventW;
 use std::ptr;
-
-use super::utils::win32_error_to_windows_error;
 
 pub fn create_event() -> windows::Result<HANDLE> {
     let handle = unsafe { CreateEventW(ptr::null(), false, false, None) };
     if handle == HANDLE(0) {
-        let err = unsafe { GetLastError() };
-        return Err(win32_error_to_windows_error(err));
+        return Err(windows::Error::from_win32());
     }
 
     Ok(handle)
--- process/src/utils.rs
+++ process/src/utils.rs
@@ -1,6 +1,5 @@
 use bindings::Windows::Win32::Foundation::{CloseHandle, HANDLE};
 use bindings::Windows::Win32::System::Com::CoUninitialize;
-use bindings::Windows::Win32::System::Diagnostics::Debug::WIN32_ERROR;
 
 pub struct CoUninitializeOnExit {}
 
@@ -18,10 +17,6 @@ impl Drop for CloseHandleOnExit {
     fn drop(&mut self) {
         unsafe { CloseHandle(self.handle) };
     }
-}
-
-pub fn win32_error_to_windows_error(err: WIN32_ERROR) -> windows::Error {
-    windows::Error::new(windows::HRESULT(0), &format!("{:#?}", err))
 }
 
 pub fn message_to_windows_error(msg: &str) -> windows::Error {
